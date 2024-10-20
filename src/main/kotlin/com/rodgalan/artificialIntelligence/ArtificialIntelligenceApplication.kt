33e671d5a0a47a4a9ae66079c82e84ceea43e32b
anna.rodriguez <anna.rodriguez@adevinta.com>
2024-10-20 18:25:46 +0200

    springboot application created

---
+++ src/main/kotlin/com/rodgalan/artificialIntelligence/ArtificialIntelligenceApplication.kt
@@ -0,0 +1,11 @@
+package com.rodgalan.artificialIntelligence
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtificialIntelligenceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArtificialIntelligenceApplication>(*args)
+}
---
+++ src/test/kotlin/com/rodgalan/artificialIntelligence/ArtificialIntelligenceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rodgalan.artificialIntelligence
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArtificialIntelligenceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
